44192fddf389e3c98fe6849fe286dfec6d00462d
Геннадий Корнеев <gena.korneev2014@gmail.com>
2025-09-19 12:22:50 +0300

    L2

--- L2.8/main.go
+++ L2.8/main.go
@@ -7,13 +7,13 @@ import (
 	"github.com/beevik/ntp"
 )
 
-// Функция NPT сервер  обернута  для проверки vet  и идиомантичности golint
+// Функция NPT сервер  обернута  для проверки vet  и golint
 func NTPTime() error {
 	currentTime, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
 	if err != nil {
 		return fmt.Errorf("failed to get time: %w", err)
 	}
-	fmt.Println("Current time:", currentTime)
+	fmt.Println("Текущее время :", currentTime)
 	return nil
 }
 
